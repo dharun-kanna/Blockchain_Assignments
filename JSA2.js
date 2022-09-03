96fd41278e5f024f8e6af8ec6136cef8a2220ece
Dharun kanna <dharunkanna117@gmail.com>
2022-09-03 21:39:44 +0530

    A2 completed

--- JSA2.js
+++ JSA2.js
@@ -8,8 +8,17 @@ function triangleType(a,b,c){
     {
      console.log(`Equilateral`);
     }
+    else if(a==b || b==c || c==a)
+    {
+        console.log(`Isosceles`)
+    }
+    else{
+        console.log(`Scalane`)
+    }
 }
 triangleType(60,60,60)
+triangleType(45,45,90)
+triangleType(30,60,90)
 
 // 2. Write a function using switch case to find the grade of a student based
 // on marks obtained
@@ -21,8 +30,40 @@ triangleType(60,60,60)
 // f. “E grade” if the marks are between 40 and 50.
 // g. “Student has failed” if the marks are between 0 and 40.
 // h. Else output “Invalid marks”.
-
-
+function grade(m){
+    let grade;
+    if(m<=100 && m>=90) grade="S";
+    else if(m<=90 && m>=80) grade="A";
+    else if(m<=80 && m>=70) grade="B";
+    else if(m<=70 && m>=60) grade="C";
+    else if(m<=60 && m>=50) grade="D";
+    else if(m<=50&& m>=40)  grade="E";
+    else if(m<=40 && m>=0)  grade="F";
+    else grade="I";
+    switch(grade)
+    {
+        case "S":
+            console.log("S grade");break;
+        case "A":
+            console.log("A grade");break;
+        case "B":
+            console.log("B grade");break;
+        case "C":
+            console.log("C grade");break;
+        case "D":
+            console.log("D grade");break;
+        case "E":
+            console.log("E grade");break;
+        case "F":
+            console.log("Student has failed");break;
+        default:
+            console.log("Invalid marks");
+    }
+}
+grade(45)
+grade(78)
+grade(94)
+grade(-4)
 
 // 3. Write a JavaScript program to find the sum of the multiples of 3 and 5
 // under 1000
@@ -48,6 +89,7 @@ function sum(n=1000){
 }
 
 sum();
+
 // 4. Write a program to find the factorial of all prime numbers between a
 // given range . Range will be passed as 2 values in the function
 // parameters. eg- if it is needed to find the values for numbers 1-100, then
