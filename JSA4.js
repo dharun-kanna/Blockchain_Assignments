1d7c6077a33ec04ad8d6776f3af825fb2a40098d
Dharun kanna <dharunkanna117@gmail.com>
2022-09-04 10:04:40 +0530

    A4 completed

---
+++ JSA4.js
@@ -0,0 +1,13 @@
+const findMatchsticks = (num) =>{
+    if (num > 1)    
+        console.log(num*6-(num-1))
+    else
+        console.log(6)
+    
+}
+
+findMatchsticks(1)
+findMatchsticks(4)
+findMatchsticks(87)
+
+
